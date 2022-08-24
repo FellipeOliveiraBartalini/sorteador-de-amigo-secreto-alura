c508977cdac46f8b4d9c7b22a8686b10f81f52a8
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-24 14:11:22 -0300

    Add Router and Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import Formulario from './components/formulario';
 import Header from './components/header';
 
 function App() {
   return (
     <>
-      <Header />
-      <App />
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={<Formulario />} />
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
     </>
   );
 }
