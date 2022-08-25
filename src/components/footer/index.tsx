8555c49e03faa04de305413a23e3b4ac2a20f040
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-25 15:52:23 -0300

    Test se sorteio foi feito

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useListaDeParticipantes } from '../../state/hooks/useListaDeParticipantes';
+import { useSorteador } from '../../state/hooks/useSorteador';
 import style from './Footer.module.scss';
 
 export default function Footer() {
@@ -9,7 +10,10 @@ export default function Footer() {
 
     const navigate = useNavigate();
 
+    const sortear = useSorteador();
+
     const iniciar = () => {
+        sortear();
         navigate('/sorteio');
     }
 
